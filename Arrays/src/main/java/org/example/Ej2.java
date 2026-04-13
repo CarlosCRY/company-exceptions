28d51c33ed2c4cda584f576127b53e66351502a0
CarlosCRY <153380227+CarlosCRY@users.noreply.github.com>
2026-04-13 11:12:20 +0200

    Delete Arrays directory

--- Arrays/src/main/java/org/example/Ej2.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class Ej2 {
-    public static void main (String[] args) {
-        double[] floats = {3.4, 5.2, 4.7};
-        System.out.println(floats[floats.length - 1]);
-    }
-}
